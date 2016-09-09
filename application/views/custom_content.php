c13af1c4352913f14c3375dac0c00647b478bce8
Arit143 <coolarit143@gmail.com>
2016-09-09 10:02:01 +0530

    Content Management App

---
+++ application/views/custom_content.php
@@ -0,0 +1,15 @@
+<link rel="stylesheet" type="text/css" href="css/customContent.css">
+
+<div class="col-sm-12 headerParagraphStyle">
+	<div class="icon"></div><h1 class = "headerStyle">WELCOME TO NATURE BLOG</h1>
+
+	<p class = "paragraphStyle" >The Earth is the only planet which man can ever call home.
+		Therefore, he must love and take care of it. 
+		This is the place where plants and animals thrive, its atmosphere and its terrain providing what man needs in order to live.
+	</p>
+</div>
+<div class = "wrapperContainerStyle">
+    <div id="customContentApp"></div>
+</div>
+
+<script src="/js/customContent/public/build/bundle.js"></script>
---
+++ application/views/default.php
@@ -0,0 +1,9 @@
+<head>
+<meta name="viewport" content="width=device-width" />
+<link rel="stylesheet" type="text/css" href="css/default.css">
+<link rel="stylesheet" type="text/css" href="js/customContent/node_modules/bootstrap/dist/css/bootstrap.min.css">
+</head>
+
+<!-- Default JS files-->
+<script src="js/customContent/node_modules/jquery/dist/jquery.min.js" type="text/javascript" charset="utf-8"></script>
+<script src="js/customContent/node_modules/bootstrap/dist/js/bootstrap.min.js" type="text/javascript" charset="utf-8"></script>
